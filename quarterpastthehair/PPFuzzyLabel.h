274c7be37f38e900ebef3b5f9eca3c90e87bc250
Matthieu Cormier <mcormier@cs.dal.ca>
2014-04-13 21:09:46 -0300

    add stub class

---
+++ quarterpastthehair/PPFuzzyLabel.h
@@ -0,0 +1,17 @@
+//
+//  PPFuzzyLabel.h
+//  quarterpastthehair
+//
+//  Created by Matthieu Cormier on 4/13/14.
+//  Copyright (c) 2014 Matthieu Cormier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PPFuzzyLabel : NSObject
+
+  +(id)labelFor:(NSInteger)minute;
+
+@end
+
+
---
+++ quarterpastthehair/PPFuzzyLabel.m
@@ -0,0 +1,19 @@
+//
+//  PPFuzzyLabel.m
+//  quarterpastthehair
+//
+//  Created by Matthieu Cormier on 4/13/14.
+//  Copyright (c) 2014 Matthieu Cormier. All rights reserved.
+//
+
+#import "PPFuzzyLabel.h"
+
+@implementation PPFuzzyLabel
+
++(id)labelFor:(NSInteger)minute {
+  
+  return nil;
+  
+}
+
+@end
